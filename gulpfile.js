d94873b4d3efff56708eb98613dc4a98ab0bb664
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-02-08 15:02:04 +0000

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var shell = require('gulp-shell');
  
-gulp.task('deploy', function() {
+
+gulp.task('build', shell.task( 'gitbook build'));
+gulp.task('deploygb', shell.task( 'git push gitbook master'));
+
+gulp.task('deploygh', [ 'build'], function() {
   return gulp.src('./_book/**/*')
     .pipe(ghPages());
 });
